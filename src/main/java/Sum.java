0a97131f77417feb76cf02927d295f92bcf1e3b1
nineclown <oring315@gmail.com>
2018-03-19 19:55:38 +0900

    Junit test

--- src/main/java/Sum.java
+++ src/main/java/Sum.java
@@ -1,18 +1,18 @@
 public class Sum {
 
-    private int sum(int sum1, int sum2) {
+    public int sum(int sum1, int sum2) {
         return sum1 + sum2;
     }
 
-    private int min(int min1, int min2) {
+    public int min(int min1, int min2) {
         return min1 - min2;
     }
 
-    private int mul(int mul1, int mul2) {
+    public int mul(int mul1, int mul2) {
         return mul1 - mul2;
     }
 
-    private int div(int div1, int div2) {
+    public int div(int div1, int div2) {
         return div1 / div2;
     }
 
