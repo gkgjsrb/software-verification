73006f40ca42dc7fb3105d21a75688f22545ad22
nineclown <oring315@gmail.com>
2018-03-19 19:29:16 +0900

    build in Intellij

---
+++ src/main/java/Sum.java
@@ -0,0 +1,23 @@
+public class Sum {
+
+    private int sum(int sum1, int sum2) {
+        return sum1 + sum2;
+    }
+
+    private int min(int min1, int min2) {
+        return min1 - min2;
+    }
+
+    private int mul(int mul1, int mul2) {
+        return mul1 - mul2;
+    }
+
+    private int div(int div1, int div2) {
+        return div1 / div2;
+    }
+
+
+    public static void main(String[] args) {
+        Sum sum = new Sum();
+    }
+}
